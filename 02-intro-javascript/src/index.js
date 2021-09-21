c1172f0c98d0e2624e5a1dc6b3e422f97cc63573
MB <milthonbor@gmail.com>
2021-09-21 00:58:31 -0700

    var const let

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Variables let const y var
+
+
+const nombre = 'Fermin';
+const apellido = 'El bueno'
+
+
+let valorDado = 5;
+
+
+console.log(nombre, apellido, valorDado);
+
+
+
+if( true ){
+    //Variables de Scope
+    //Solo funcionan detro del scope
+    let valorDado = 6;
+
+    console.log(valorDado);
+}
+
+
+
+console.log(valorDado);
