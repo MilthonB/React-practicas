2693f69956bd667be20c01deccaaae683c3f475c
MB <milthonbor@gmail.com>
2021-09-21 18:23:56 -0700

    async await

---
+++ 02-intro-javascript/src/base/11-async-await.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+const getImgen = async () => {
+
+    const resp = await fetch('http://api.giphy.com/v1/gifs/random?api_key=FAojj336ohZpReqVhyVwVOHo1jWO94vB&limit=10');
+
+
+    const { data } = await resp.json();
+    const { url } = data.images.original
+
+    console.log(url)
+
+    const img = document.createElement('img');
+    img.src = url;
+
+    document.body.append(img);
+
+
+
+}
+
+
+getImgen();
+
+
+// promesa
+// .then( resp =>  resp.json() )
+// .then( ({data}) => {
+
+//     const { url } = data.images.original;
+//     const img = document.createElement('img');
+//     img.src = url;
+
+//     document.body.append(img);
+
+// })
+// .catch( console.warn)
