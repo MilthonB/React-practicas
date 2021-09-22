f8b598ad0c85eb5536ea567551cd5ccba424e879
MB <milthonbor@gmail.com>
2021-09-21 18:32:15 -0700

    Js terminado

--- 02-intro-javascript/src/base/12-operador-ternario.js
+++ 02-intro-javascript/src/base/12-operador-ternario.js
@@ -1,10 +1,9 @@
 
-
-
 const activo = true;
 
 let mensaje = '';
 
+//Forma tradicional 
 if( activo ){
     mensaje="Activo";
 }else{
@@ -14,3 +13,14 @@ if( activo ){
 
 console.log(mensaje)
 
+
+//Condicion ternaria 
+// mensaje = (activo) ? 'Activo' : 'Inactivo';
+// mensaje = (!activo) ? 'Activo' : null;
+
+//condicion and
+mensaje = activo && 'Activo'; //Mensaje activo
+// mensaje = !activo && 'Activo'; //falso retorna
+
+
+console.log('Mensaje dos ',mensaje)
