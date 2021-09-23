43dae68b03ad1c83bbbfe580f9d6f7c35f56dcfb
MB <milthonbor@gmail.com>
2021-09-22 19:35:56 -0700

    Event onClick

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -3,10 +3,18 @@ import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
 
+    const handlApp = (e) => {
+        console.log(e)
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2> {value} </h2>
+
+            <button onClick={ handlApp } >
+                +1
+            </button>
         </>
     );
 
@@ -18,3 +26,4 @@ CounterApp.propTypes = {
 
 
 export default CounterApp;
+
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,7 +1,7 @@
 
 import  React  from "react";
 import ReactDOM from "react-dom";
-import PrimeraApp from "./PrimerApp";
+// import PrimeraApp from "./PrimerApp";
 import './index.css';
 import CounterApp from "./CounterApp";
 
