74d3e8de6bb21dad28020f36d2f2243aa5cd82b8
MB <milthonbor@gmail.com>
2021-09-22 18:43:27 -0700

    Función

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -3,10 +3,11 @@ import  React  from "react";
 import ReactDOM from "react-dom";
 import PrimeraApp from "./PrimerApp";
 import './index.css';
+import CounterApp from "./CounterApp";
 
 
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( <PrimeraApp saludo = {123} />, divRoot );
+ReactDOM.render( <CounterApp value = {123} />, divRoot );
 
