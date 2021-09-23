87852c0cb12b0ea9e4844c175a935764ceab1e9a
MB <milthonbor@gmail.com>
2021-09-22 17:38:33 -0700

    primer componente

---
+++ 03-counter-app/src/PrimerApp.js
@@ -0,0 +1,17 @@
+
+// import React from 'react';
+
+
+//Exiten dos tipos de componentes 
+//Los que estan basdos en clases y otros en funciones
+
+//Fcuntional component
+
+const PrimeraApp = () => {
+
+
+    return <h1>Hola mundo</h1>;
+
+}
+
+export default PrimeraApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,11 +1,12 @@
 
 import  React  from "react";
 import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimerApp";
+import './index.css';
 
 
-const saludo = <h1>Hola mundo</h1>;
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( saludo, divRoot );
+ReactDOM.render( <PrimeraApp />, divRoot );
 
