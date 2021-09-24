2d3328d0edea93f4e11c6e1d71dfa56e870d5b19
MB <milthonbor@gmail.com>
2021-09-23 20:32:19 -0700

    typeof

--- 04-pruebas-counter-app/src/tests/base/06-desestucturacion-arreglos.test.js
+++ 04-pruebas-counter-app/src/tests/base/06-desestucturacion-arreglos.test.js
@@ -8,8 +8,11 @@ describe('Pruebas 06-desestructuracion-arr.js', () => {
 
         const [letras, numeros] = retornaArreglo();
 
-         expect(letras).toBe('ABC');
+        expect(letras).toBe('ABC');
+        expect( typeof letras).toBe('string');
+         
          expect(numeros).toBe(123);
+         expect( typeof numeros).toBe('number');
 
     });
     
