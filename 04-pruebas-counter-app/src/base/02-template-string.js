b026137d9d66bf22a9643386ed3773fae2a0d559
MB <milthonbor@gmail.com>
2021-09-23 19:48:17 -0700

    pruebas teamplate string

--- 04-pruebas-counter-app/src/base/02-template-string.js
+++ 04-pruebas-counter-app/src/base/02-template-string.js
@@ -4,10 +4,10 @@ const nombre= "Felizardo";
 const apellido = "Salamanca";
 
 
-const getSaludo = () => {
-    return 'Hola estimado';
+export const getSaludo = (nombre='Carlos') => {
+    return 'Hola '+nombre;
 }
 
 
-const nombreCompleto = `${ getSaludo() } ${nombre} ${apellido} `
+const nombreCompleto = `${ getSaludo(nombre) } ${apellido} `
 console.log(nombreCompleto);
