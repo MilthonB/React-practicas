b611e1bd9ab583e04d773485cd682ad10a99c19c
MB <milthonbor@gmail.com>
2021-09-23 19:22:56 -0700

    pruebas jest exoect tobe describe

---
+++ 04-pruebas-counter-app/src/tests/demo.test.js
@@ -0,0 +1,17 @@
+
+
+
+describe('Pruebas en el archivo demo.test.js', ()=> {
+
+    test('debe ser string los dos', () => {
+    
+        const mensaje = 'Hola mundo';
+    
+        const mensaje2 = "Hola mundo!";
+    
+        expect(mensaje).toBe(mensaje2);
+    
+    });
+})
+
+//Test
