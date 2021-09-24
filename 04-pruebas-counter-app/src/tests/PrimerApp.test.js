40ecfa57be3ff0c8a83cd390b3e4db7d99b0140c
MB <milthonbor@gmail.com>
2021-09-24 16:53:06 -0700

    Enzyme pruebas realizadas

--- 04-pruebas-counter-app/src/tests/PrimerApp.test.js
+++ 04-pruebas-counter-app/src/tests/PrimerApp.test.js
@@ -1,20 +1,38 @@
 import { render } from "@testing-library/react";
-import PrimeraApp from "../PrimerApp";
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+Enzyme.configure({ adapter: new Adapter() });
 // import "@testing-library/jest-dom/extend-expect";
 
+
+import PrimeraApp from "../PrimerApp";
 
 
 
 describe('Pruebas a PrimerApp.js', () => {
 
 
-    test('debe de retornar un Hola soy goku', () => {
+    // test('debe de retornar un Hola soy goku', () => {
+
+    //     const saludo = 'Hola soy goku';
+
+    //     const { getByText } = render(<PrimeraApp saludo = {saludo} />);
+        
+    //      expect(getByText(saludo)).toBeInTheDocument();
+
+
+    // });
+
+
+    test('debe de mostrar <PrimerApp /> correctamente', () => {
+
 
         const saludo = 'Hola soy goku';
+        const wrapper = shallow( <PrimeraApp saludo={saludo} /> )
 
-        const { getByText } = render(<PrimeraApp saludo = {saludo} />);
+        // const { getByText } = render(<PrimeraApp saludo = {saludo} />);
         
-         expect(getByText(saludo)).toBeInTheDocument();
+        //  expect(getByText(saludo)).toBeInTheDocument();
 
 
     });
