95e8d1400025c26071149a13b820a97cb5ec79de
MB <milthonbor@gmail.com>
2021-09-24 19:58:36 -0700

    setcategoria mandar elemento al arreglo de cabezer

--- 05-gif-expert-app/src/GifExpertApp.js
+++ 05-gif-expert-app/src/GifExpertApp.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 
 
 export const GifExpertApp = () => {
+
+
+    // const categorias = ['DBZ', 'Pokemon','Gladiadores'];
+    const [categorias, setCategorias] = useState(['DBZ', 'Pokemon', 'Gladiadores']);
+
+    const handleAdd = () => {
+
+        // setCategorias(categorias => [...categorias,'Malcomn']);
+        setCategorias([...categorias,'Malcomn']);
+    }
 
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <button onClick={handleAdd} >Agregar</button>
+
+            <ol>
+                {
+                    categorias.map(category => {
+                        return <li key={category} > {category} </li>
+                    })
+                }
+            </ol>
+
         </>
     );
 
