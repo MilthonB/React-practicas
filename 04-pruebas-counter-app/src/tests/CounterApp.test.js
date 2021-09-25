59f847d268b9a4c7e6737ca32248551bfb4ffbff
MB <milthonbor@gmail.com>
2021-09-24 18:30:00 -0700

    pruebas button click event

--- 04-pruebas-counter-app/src/tests/CounterApp.test.js
+++ 04-pruebas-counter-app/src/tests/CounterApp.test.js
@@ -5,10 +5,16 @@ import CounterApp from "../CounterApp";
 
 describe('Pruebas a CounterApp.js', () => {
 
-    test('debe mostrar el <CounterApp /> correctamente', () => {
-        
+    let wrapper = shallow(<CounterApp />);
+    
+    beforeEach( () => {
+        wrapper = shallow(<CounterApp />);
+    } );
 
-        const wrapper = shallow( <CounterApp /> );
+
+    test('debe mostrar el <CounterApp /> correctamente', () => {
+
+
         expect(wrapper).toMatchSnapshot();
 
 
@@ -16,20 +22,41 @@ describe('Pruebas a CounterApp.js', () => {
 
 
     test('debe mostrar el valor por defecto de 100', () => {
-        
+
         const value = 100;
 
-        const wrapper = shallow( <CounterApp value={100}/> );
-        
+        const wrapper = shallow(<CounterApp value={100} />);
+
         const textH2 = wrapper.find('h2').text();
 
-         expect(textH2).toBe('100');
-        
+        expect(textH2).toBe('100');
+
 
 
 
     });
-    
-    
+
+
+    test('debe de incrementar con el boton +1', () => {
+
+        const btn = wrapper.find('button').at(0).simulate('click');
+        const counter = wrapper.find('h2').text().trim();
+
+        expect(counter).toBe('11')
+
+
+    });
+
+
+    test('debe de decrementar con el boton -1', () => {
+
+        const btn = wrapper.find('button').at(2).simulate('click');
+        const counter = wrapper.find('h2').text().trim();
+
+        expect(counter).toBe('9')
+
+
+    });
+
 
 });
